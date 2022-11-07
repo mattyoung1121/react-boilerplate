29c65e00f232a826d70166a005bd669d062f45fa
Matthew Young <matthew.young@ramseysolutions.com>
2022-11-07 15:12:22 -0600

    react boilerplate

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+const App = () => (
+  <div className="app">
+    <header className="app-header">
+      <h1>New React App</h1>
+    </header>
+  </div>
+);
+
+export default App;
